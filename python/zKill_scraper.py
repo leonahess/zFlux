5c41d63998eb466c79fb2f7521457c4b4e67ec1a
Leon Hess <32615985+leonhess@users.noreply.github.com>
2018-06-30 19:17:05 +0200

    cleaned code and removed unused methods (attacker name fetching still not fully implemented again)

--- python/zKill_scraper.py
+++ python/zKill_scraper.py
@@ -21,99 +21,48 @@ def EsiCall(ids):
     else:
         names = []
 
-    logger.debug("attacker_name: {}".format(names))
-
     return names
-
-
-'''
-def UniquifyingIds(dict, kind):
-    attacker_id = []
-    for t in range(0, len(dict['package']['killmail']['attackers'])):
-        if "{}_id".format(kind) in dict['package']['killmail']['attackers'][t] and dict['package']['killmail']['attackers'][t]['{}_id'.format(kind)] not in attacker_id:
-            attacker_id.append(dict['package']['killmail']['attackers'][t]['{}_id'.format(kind)])
-    return attacker_id
-
-def DictToString(attacker_name_dict, kind):
-    attacker_name_str = ';'
-    for t in range(0, len(attacker_name_dict)):
-        try:
-            attacker_name_str = attacker_name_str + attacker_name_dict[t]['{}_name'.format(kind)] + ';'
-        except KeyError:
-            logger.warning("KeyError: {}".format(attacker_name_str))
-    return attacker_name_str
-'''
 
 
 def FetchVictimNameWithId(killmail):
 
-    if 'character_id' in killmail['package']['killmail']['victim']:
-        char_id = killmail['package']['killmail']['victim']['character_id']
-    else:
-        char_id = ''
-
-    if 'corporation_id' in killmail['package']['killmail']['victim']:
-        corp_id = killmail['package']['killmail']['victim']['corporation_id']
-    else:
-        corp_id = ''
-
-    if 'alliance_id' in killmail['package']['killmail']['victim']:
-        alliance_id = killmail['package']['killmail']['victim']['alliance_id']
-    else:
-        alliance_id = ''
-
     victim_ids = []
 
-    if char_id != '':
-        victim_ids.append(char_id)
-    if corp_id != '':
-        victim_ids.append(corp_id)
-    if alliance_id != '':
-        victim_ids.append(alliance_id)
+    if 'character_id' in killmail['package']['killmail']['victim']:
+        victim_ids.append(killmail['package']['killmail']['victim']['character_id'])
+
+    if 'corporation_id' in killmail['package']['killmail']['victim']:
+        victim_ids.append(killmail['package']['killmail']['victim']['corporation_id'])
+
+    if 'alliance_id' in killmail['package']['killmail']['victim']:
+        victim_ids.append(killmail['package']['killmail']['victim']['alliance_id'])
 
     names = EsiCall(victim_ids)
 
-    logger.debug("Victim char_id: {}".format(char_id))
-    logger.debug("Victim corp_id: {}".format(corp_id))
-    logger.debug("Victim alli_id: {}".format(alliance_id))
     logger.debug("Victim ids: {}".format(victim_ids))
     logger.debug("Victim names: {}".format(names))
 
     return names
-'''
-def GetName(dict, kind):
-    attacker_ID = UniquifyingIds(dict, '{}'.format(kind))
-    victim_ID = []
-    logger.debug("attacker_{}ID: {}".format(kind, attacker_ID))
 
-    if '{}_id'.format(kind) in dict['package']['killmail']['victim']:
-        victim_ID.append(dict['package']['killmail']['victim']['{}_id'.format(kind)])
-        logger.debug("victim_{}ID: {}".format(kind, victim_ID))
-        all_ID =
-    else:
-        all_ID = attacker_ID
-    logger.debug("all_{}ID: {}".format(kind, all_ID))
 
-    if all_ID:
-        all_name_dict = FetchNameWithId(all_ID, '{}'.format(kind))
-        logger.debug("all_{}_name_dict: {}".format(kind, all_name_dict))
-        attacker_name_dict = all_name_dict
-        victim_name = ''
+def FetchAttackerNameWithId(killmail):
 
-        if victim_ID and all_name_dict:
-            victim_name = all_name_dict[-1]['{}_name'.format(kind)]
-            logger.debug("victim_{}_name: {}".format(kind, victim_name))
-            attacker_name_dict = all_name_dict[:-1]
+    attacker_ids = []
 
-        attacker_name = DictToString(attacker_name_dict, '{}'.format(kind))
-        logger.debug("attacker_{}_name: {}".format(kind, attacker_name))
-        final_name_dict = {"victim_name": victim_name, "attacker_name": attacker_name}
-    else:
-        final_name_dict = {"victim_name": '', "attacker_name": ''}
+    for entry in killmail['package']['killmail']['attackers']:
+        if 'character_id' in entry and entry['character_id'] not in attacker_ids:
+            attacker_ids.append(entry['character_id'])
+        if 'corporation_id' in entry and entry['corporation_id'] not in attacker_ids:
+            attacker_ids.append(entry['corporation_id'])
+        if 'alliance_id' in entry and entry['alliance_id'] not in attacker_ids:
+            attacker_ids.append(entry['alliance_id'])
 
-    logger.debug("final_name_dict: {}".format(final_name_dict))
-    return final_name_dict
-'''
+    names = EsiCall(attacker_ids)
+
+    logger.debug("Attacker attacker_ids: {}".format(attacker_ids))
+    logger.debug("Attacker names: {}".format(names))
+
+    return names
 
 
 def GetAttackerShips(dict):
@@ -123,7 +72,7 @@ def GetAttackerShips(dict):
 
     for x in range(0, len(dict['package']['killmail']['attackers'])):
         try:
-            if(dict['package']['killmail']['attackers'][x]['ship_type_id'] not in attacker_ship_list):
+            if dict['package']['killmail']['attackers'][x]['ship_type_id'] not in attacker_ship_list:
                 attacker_ship_list.append(dict['package']['killmail']['attackers'][x]['ship_type_id'])
         except KeyError:
             logger.error("attacker ship key error")
@@ -133,7 +82,7 @@ def GetAttackerShips(dict):
         inv_types = csv.reader(f)
         for row in inv_types:
             for l in range(0, len(attacker_ship_list)):
-                if(row[0] == str(attacker_ship_list[l])):
+                if row[0] == str(attacker_ship_list[l]):
                     attacker_ship_name_list.append(row[2])
     logger.debug("Ship name list: {}".format(attacker_ship_name_list))
 
@@ -146,8 +95,12 @@ def GetAttackerShips(dict):
 
 
 start = datetime.datetime.now()
+last100_processing_time = []
+counter = 1
 
-logging.basicConfig(filename ='zKill_scraper_{}.log'.format(start), level = logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
+
+# start logger
+logging.basicConfig(filename='zKill_scraper_{}.log'.format(start), level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 logger = logging.getLogger(__name__)
 
 # Connect to Database and check if database 'eve' exists, otherwise create one
@@ -157,14 +110,12 @@ eve_exists = False
 for s in range(0, len(database_list)):
     if database_list[s]['name'] == 'eve':
         eve_exists = True
-if eve_exists == False:
+if not eve_exists:
     client.create_database('eve')
 
-last100_processing_time = []
-counter = 1
 while True:
     try:
-        r = requests.get('https://redisq.zkillboard.com/listen.php?queueID=zKill_scaperDev', timeout = 20)
+        r = requests.get('https://redisq.zkillboard.com/listen.php?queueID=zKill_scaperDev', timeout=20)
     except requests.exceptions.Timeout:
         logger.warning("zKill request timed out")
     except requests.exceptions.RequestException as e:
@@ -179,7 +130,7 @@ while True:
     if r.text == '{"package":null}':
         logger.info(r.text)
 
-    if dict['package'] != None:
+    if dict['package'] is not None:
 
         # METADATA
         killID = dict['package']['killID']
@@ -206,8 +157,9 @@ while True:
                 if row[0] == str(victim_shipID):
                     victim_ship_name = row[2]
 
-        # GET VICTIM NAMES
+        # GET NAMES FROM IDs
         victim_names = FetchVictimNameWithId(dict)
+        attacker_names = FetchAttackerNameWithId(dict)
 
         # CHARACTER IDs
         attacker_char_name = ''
@@ -233,31 +185,29 @@ while True:
             if entry['category'] == 'alliance':
                 victim_alliance_name = entry['name']
 
-
-
-        # logger
+        # LOGGER
         logger.info("killID: {}".format(killID))
         logger.info("killmail_time: {}".format(killmail_time))
-        logger.info("solar_systemID: {}".format(solar_systemID))
-        logger.info("totalValue: {}".format(totalValue))
+        logger.debug("solar_systemID: {}".format(solar_systemID))
+        logger.debug("totalValue: {}".format(totalValue))
 
-        logger.info("attackers_amount: {}".format(attacker_amount))
-        logger.info("attacker_is_npc: {}".format(attacker_is_npc))
-        logger.info("attacker_is_solo: {}".format(attacker_is_solo))
-        logger.info("attacker_is_awox: {}".format(attacker_is_awox))
-        logger.info("attacker_char_name: {}".format(attacker_char_name))
-        logger.info("attacker_corp_name: {}".format(attacker_corp_name))
-        logger.info("attacker_alli_name: {}".format(attacker_alliance_name))
+        logger.debug("attackers_amount: {}".format(attacker_amount))
+        logger.debug("attacker_is_npc: {}".format(attacker_is_npc))
+        logger.debug("attacker_is_solo: {}".format(attacker_is_solo))
+        logger.debug("attacker_is_awox: {}".format(attacker_is_awox))
+        logger.debug("attacker_char_name: {}".format(attacker_char_name))
+        logger.debug("attacker_corp_name: {}".format(attacker_corp_name))
+        logger.debug("attacker_alli_name: {}".format(attacker_alliance_name))
 
-        logger.info("victim_damage_taken: {}".format(victim_damage_taken))
-        logger.info("victim_ship_name: {}".format(victim_ship_name))
-        logger.info("victim_char_name: {}".format(victim_char_name))
-        logger.info("victim_corp_name: {}".format(victim_corp_name))
-        logger.info("victim_alli_name: {}".format(victim_alliance_name))
+        logger.debug("victim_damage_taken: {}".format(victim_damage_taken))
+        logger.debug("victim_ship_name: {}".format(victim_ship_name))
+        logger.debug("victim_char_name: {}".format(victim_char_name))
+        logger.debug("victim_corp_name: {}".format(victim_corp_name))
+        logger.debug("victim_alli_name: {}".format(victim_alliance_name))
 
         # ASSEMBLING NEW JSON BODY
-        json_body = [{"measurement":"kills",
-            "tags":{
+        json_body = [{"measurement": "kills",
+            "tags": {
                     "solar_systemID": solar_systemID,
 
                     "attacker_is_npc": attacker_is_npc,
@@ -272,15 +222,15 @@ while True:
                     "victim_corp_name": victim_corp_name,
                     "victim_alliance_name": victim_alliance_name,
                     "victim_ship_name": victim_ship_name
-                },
-            "fields":{
-                    "#kills":1,
+            },
+            "fields": {
+                    "#kills": 1,
                     "totalValue": totalValue,
                     "attacker_amount": attacker_amount,
                     "victim_damage_taken": victim_damage_taken,
-                },
+            },
             "time": killmail_time,
-            "time_prescision":"s"
+            "time_prescision": "s"
         }]
 
         # WRITING TO DATABASE
